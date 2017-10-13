686c0375bbf8eba405cc235ffb9bb6abbbb35ef0
Jesse Firestone <firestonej@gmail.com>
2017-10-13 16:13:35 -0700

    Major overhaul to worker; add Promise logic to ensure authentication

--- worker.js
+++ worker.js
@@ -1,33 +1,92 @@
 var fetch = require('node-fetch');
 var fs = require('fs');
+var async = require('async-polling');
 
-authUrl = 'https://api.experienteventbit.com/API/AuthUser';
+// Constants
+var baseUrl = 'https://api.experienteventbit.com/';
 
-authOpts = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify( { Username: "EXPOREG\\development", Password: "EDU171api" } )
-};
+/**
+ * Follows instructions at https://api.experienteventbit.com/docs/
+ * to sign in appropriately.
+ */
+function getAuth() {
+  authUrl = baseUrl + 'API/AuthUser';
 
-fetch(authUrl, authOpts)
-  .then(function(res) {
+  credentials = { Username: process.env.experientUsername, Password: process.env.experientPassword };
 
-    console.log(res.headers['_headers']);
+  authOpts = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  };
 
-    url = 'https://api.experienteventbit.com/api/event/EDU161/RptSessionPerformance?IsAttendeeOnly=true&IsNotBoothPersonnel=true&IsNotEstimated=false&IsRegistered=false&IsVerified=false&RangeStart=2016-10-27T19:56:25.000Z&RangeEnd=2016-10-27T20:11:25.000Z';
+  return [ authUrl, authOpts ];
+
+}
+
+function grab(authString) {
+  return new Promise(function(resolve, reject) {
+    url = baseUrl + 'api/event/EDU161/RptSessionPerformance?IsAttendeeOnly=true&IsNotBoothPersonnel=true&IsNotEstimated=false&IsRegistered=false&IsVerified=false&RangeStart=2016-10-27T19:56:25.000Z&RangeEnd=2016-10-27T20:11:25.000Z';
 
     opts = {
-      headers: { 'X-AUTH-CLAIMS': res.headers['_headers']['x-auth-claims'] }
+      headers: { 'X-AUTH-CLAIMS': authString }
     };
 
-    console.log(opts);
+    console.log('Attempting to authenticate with header ' + authString);
 
     fetch(url, opts)
-      .then(function(res) {
-        var dest = fs.createWriteStream('public/data/traffic.json');
-        res.body.pipe(dest);
-      });
+      .then(function(response) {
+        if (response.status == 200) {
+          var dest = fs.createWriteStream('public/data/traffic.json');
+          response.body.pipe(dest);
+          console.log('Successfully wrote new JSON.');
+          resolve(); // @todo: Add timestamp
+        }
 
+        else if (response.status == 400) {
+          console.log('Authentication needed.');
+          reject();
+        }
+
+      });
   });
+}
+
+var authString = '';
+
+var polling = async(function(end) {
+  console.log(authString);
+
+  grab(authString).then(
+    function(response) {
+      console.log('Wrote successfully');
+    },
+    function(error) {
+      auth = getAuth();
+
+      fetch(auth[0], auth[1])
+        .then(function(res) {
+          console.log(res.headers);
+          if (res.status == 200) {
+            console.log(res.headers['_headers']);
+
+            return res.headers['_headers']['x-auth-claims'];
+          }
+        })
+        .then(function(newAuth) {
+          authString = newAuth;
+        });
+    }
+  );
+
+  end();
+}, 5000);
+
+
+polling.on('error', function (error) {
+  console.error('error:', error);
+});
+
+polling.run();
