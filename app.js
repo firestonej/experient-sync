74da321f273faf53fd9f648e1e0e58e3f3c4db3d
Jesse Firestone <firestonej@gmail.com>
2017-10-17 15:01:09 -0700

    Major app overhaul to join sessions-traffic, sorting implementation on back-end

--- app.js
+++ app.js
@@ -30,8 +30,42 @@ app.Session = Backbone.Model.extend({
   defaults: {
     Code: '',
     Title: '',
+    CurrentTraffic: 0,
+    Capacity: 0,
+    Fullness: '',
+    FullnessPercentage: 0
   },
   idAttribute: 'Code',
+  initialize: function() {
+    this.on('change', this.setFullness);
+  },
+  setFullness: function(changes) {
+    try {
+      // Peter's magical
+      var ratio = (this.get('CurrentTraffic') * 1.4) / this.get('Capacity');
+      var fullness = 'Full';
+
+      if (ratio <= .25) {
+        fullness =  'Open';
+      }
+      else if (ratio < .5) {
+        fullness =  'Medium';
+      }
+      else if (ratio < .75) {
+        fullness =  'Busy';
+      }
+      else if (fullness < .95) {
+        fullness =  'Crowded'
+      }
+
+      this.set('Class', fullness.toLowerCase());
+      this.set('Fullness', fullness);
+      this.set('FullnessPercentage', (ratio * 100).toFixed());
+    }
+    catch (e) {
+      console.error(e);
+    }
+  },
 });
 
 app.Traffic = Backbone.Model.extend({
@@ -39,6 +73,19 @@ app.Traffic = Backbone.Model.extend({
     Code: '',
     Name: '',
     Traffic: 0,
+  },
+  idAttribute: 'Code',
+  initialize: function() {
+    this.on('change:Traffic', function (thing) {
+      newAttr = this.changedAttributes();
+
+      if (this.has('SessionModel')) {
+
+        this.get("SessionModel").set("CurrentTraffic", newAttr.Traffic);
+      }
+
+
+    });
   }
 });
 
@@ -63,7 +110,20 @@ app.TrafficList = Backbone.Collection.extend({
 // Session metadata collection.
 app.SessionList = Backbone.Collection.extend({
   initialize: function(models, options) {
-    // this.on("add change", this.updateMetadata);
+    _.defaults(this, {
+      'sortField': 'Title',
+    });
+  },
+  comparator: function(model) {
+    if (this.sortField == 'Title') {
+      return model.get('Title').toLowerCase();
+    }
+    else if (this.sortField == 'FullnessPercentage') {
+      return model.get('FullnessPercentage');
+    }
+    else if (this.sortField == 'Room') {
+      return model.get('MeetingRoom').toLowerCase();
+    }
   },
   model: app.Session,
   url: '/public/metadata/sessions16.json',
@@ -91,15 +151,16 @@ var SessionView = Backbone.View.extend({
     ];
     this.errorEl = $('#error');
 
-    this.listenTo(this.traffic, "reset change", this.render);
-    this.listenTo(this.metadata, "reset change", this.render);
+    this.activeSessions = new app.SessionList();
+
+    this.listenTo(this.traffic, "reset change", this.filterSessions);
+    this.listenTo(this.metadata, "reset", this.joinSessions);
 
     this.metadata.fetch({reset:true});
     this.traffic.fetch({reset:true});
   },
 
-  render: function() {
-    this.$el.html('');
+  joinSessions: function() {
 
     if (this.metadata.size() == 0) {
       console.log('Metadata not loaded yet.')
@@ -108,36 +169,67 @@ var SessionView = Backbone.View.extend({
     }
 
     if (this.traffic.size() == 0) {
-      console.log('No traffic results right now.');
-      this.errorEl.html('No session traffic right now.');
+      console.log('No traffic data.');
       return;
     }
 
-    this.errorEl.html('');
-
-    this.traffic.each(function(model) {
+    this.traffic.each(function (model) {
 
       try {
         session = this.metadata.get(model.get("Code"));
 
-        if (typeof session !== "undefined" && $.inArray(session.get("Type"), this.acceptedTypes) < 0 ) {
+        if (typeof session == "undefined" || $.inArray(session.get("Type"), this.acceptedTypes) < 0) {
           // console.log(session.get("Type"));
           // console.log(this.acceptedTypes);
           return;
         }
 
-        try {
-          var output = this.template({ traffic:  model.toJSON(), metadata: session.toJSON() });
-          this.$el.append(output);
-        }
-        catch (e) {
-          // console.log("Render error");
-          // console.log(e);
-        }
+        session.set({
+          'CurrentTraffic': model.get("Traffic"),
+        });
+
+        // Link backwards for .change events
+        model.set("SessionModel", session);
+
       } catch (e) {
         // TypeError
-        console.log("Failed to load metadata for code: " + model.get("Code"));
-        console.log(e);
+        // console.log("Failed to load metadata for code: " + model.get("Code"));
+        // console.log(e);
+      }
+
+    }, this);
+  },
+
+  filterSessions: function() {
+    var filtered = this.metadata.filter(function(t) {
+      return t.get('CurrentTraffic') > 0;
+    });
+
+    this.activeSessions.reset(filtered);
+    // this.activeSessions.sort();
+
+    this.render();
+  },
+
+  render: function() {
+
+    if (this.activeSessions.size() == 0) {
+      console.log('No traffic results right now.');
+      this.errorEl.html('No session traffic right now.');
+      return;
+    }
+
+    this.$el.html('');
+    this.errorEl.html('');
+
+    this.activeSessions.each(function(model) {
+
+      try {
+        var output = this.template(model.toJSON());
+        this.$el.append(output);
+      }
+      catch (e) {
+        console.error(e);
       }
 
     }, this);
@@ -146,20 +238,31 @@ var SessionView = Backbone.View.extend({
 
 });
 
-var sessionMetadata = new app.SessionList();
+var sessionMetadata = new app.SessionList(null);
 
 var trafficList = new app.TrafficList();
 
 var sessionView = new SessionView({ traffic: trafficList, metadata: sessionMetadata });
 
 var trafficPoll = Backbone.Poller.get(trafficList, {
-  delay: 1000
+  delay: 5000,
+  continueOnError: true
 });
 
 var metadataPoll = Backbone.Poller.get(sessionMetadata, {
-  delay: 5000
+  delay: 30000
+});
+
+trafficList.on('error', function(k){
+  console.error(k);
 });
 
 
-trafficPoll.start();
+trafficPoll.on('error', function(model){
+  console.error('oops! something went wrong');
+  console.log(model);
+});
+
 metadataPoll.start();
+trafficPoll.start();
+
